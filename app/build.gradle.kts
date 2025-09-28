ba558eb7d0848a7294285cf78fb492f7451680f8
hongducdev <contact.hongduc@gmail.com>
2025-09-28 12:41:06 +0700

    Add Room dependencies and update project configurations
    This commit introduces the Room persistence library to the project.
    It also includes updates to various IDE and build configurations:
    
    - Added KSP plugin and Room dependencies to `app/build.gradle.kts` and `build.gradle.kts`.
    - Updated library versions in `gradle/libs.versions.toml`.
    - Added new IDE configuration files:
        - `.idea/appInsightsSettings.xml`
        - `.idea/codeStyles/Project.xml`
        - `.idea/codeStyles/codeStyleConfig.xml`
        - `.idea/compiler.xml`
        - `.idea/deploymentTargetSelector.xml`
        - `.idea/vcs.xml`
    - Updated existing IDE configuration files:
        - `.idea/gradle.xml`
        - `.idea/misc.xml`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -49,6 +50,15 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+//    Add the Room Dependencies
+    implementation(libs.androidx.room)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.room.testing)
+    implementation(libs.androidx.room.paging)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

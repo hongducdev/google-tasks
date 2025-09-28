7f11faeec10b14d4534e4a2cc1d16ddb21e95c35
hongducdev <contact.hongduc@gmail.com>
2025-09-28 15:40:46 +0700

    Rename TaskEntity column from 'created_at' to 'updated_at'
    This commit updates the `TaskEntity` Room entity by renaming the `created_at` column to `updated_at`. This change better reflects that the timestamp represents the last modification time of the task.

--- app/src/main/java/com/hongducdev/googletasks/database/entity/TaskEntity.kt
+++ app/src/main/java/com/hongducdev/googletasks/database/entity/TaskEntity.kt
@@ -16,7 +16,7 @@ data class TaskEntity(
     val isCompleted: Boolean,
     @ColumnInfo(name = "collection_id")
     val collectionId: Int,
-    @ColumnInfo(name = "created_at")
+    @ColumnInfo(name = "updated_at")
     val updatedAt: Long
 )
 
